c674c3e458e533f5771e609bad059e908b65f98c
Ilona <mihalenokilona@gmail.com>
2026-02-24 11:15:42 +0300

    change

--- src/main/java/com/example/photostudio/controller/PhotoSessionController.java
+++ src/main/java/com/example/photostudio/controller/PhotoSessionController.java
@@ -1,14 +1,28 @@
 package com.example.photostudio.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.photostudio.dto.PhotoSessionAdminResponseDto;
 import com.example.photostudio.dto.PhotoSessionRequestDto;
 import com.example.photostudio.dto.PhotoSessionResponseDto;
 import com.example.photostudio.service.PhotoSessionService;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
+
+
 
 @RestController
 @RequestMapping("/api/photoSession")
