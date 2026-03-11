09746dc3c032e7fa5d22246fcdfabea46663bd8b
Ilona <mihalenokilona@gmail.com>
2026-03-11 20:30:51 +0300

    corrected

--- src/main/java/com/example/photostudio/PhotostudioApplication.java
+++ src/main/java/com/example/photostudio/PhotostudioApplication.java
@@ -2,8 +2,10 @@ package com.example.photostudio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class PhotostudioApplication {
     public static void main(String[] args) {
         SpringApplication.run(PhotostudioApplication.class, args);
